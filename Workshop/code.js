1864e11a8abdad2a2ffbbd9804974633f0fa0de3
NickRiley7 <nick_riley@LAPTOP-J8V35VGC>
2023-10-05 15:48:36 -0400

    block07 Workshop

---
+++ Workshop/code.js
@@ -0,0 +1,16 @@
+/* Need to create a string for my greeting
+Then assign variables to get the numbers in the cobination
+then create an alert that tells the combination of the lock*/
+
+// Here is my greeting//
+const greeting = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+
+//Here are my variables that equal the combination//
+let num1 = 5 + 5 
+let num2 = 10 * 4 
+let num3 = 78 / 2 
+
+//Here is my alert displaying the greeting and cobination//
+alert(`${greeting} ${num1} ${num2} ${num3}`)
+
+//Create a link from index.html to code.js to be able to view in browser//
